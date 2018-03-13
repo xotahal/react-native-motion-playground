00e7d80ad2a1f8285d86b3ea3b3f28bfd4ee7b02
Jiří Otáhal <xotahal@gmail.com>
2018-03-13 15:25:32 +1300

    Add toolbar background animation

--- src/components/ToolbarBackground.js
+++ src/components/ToolbarBackground.js
@@ -1,15 +1,49 @@
 import React, { PureComponent } from 'react';
-import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import {
+  Animated,
+  Text,
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+} from 'react-native';
 
 class ToolbarBackground extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      translateY: new Animated.Value(-150),
+    };
+  }
+  componentWillReceiveProps(nextProps) {
+    if (!this.props.isDetail && nextProps.isDetail) {
+      this.showAnimation();
+    }
+    if (this.props.isDetail && !nextProps.isDetail) {
+      this.hideAnimation();
+    }
+  }
+  hideAnimation() {
+    Animated.timing(this.state.translateY, {
+      toValue: -150,
+      useNativeDriver: true,
+    }).start();
+  }
+  showAnimation() {
+    Animated.timing(this.state.translateY, {
+      toValue: 0,
+      useNativeDriver: true,
+    }).start();
+  }
   render() {
+    const { translateY } = this.state;
     const { isDetail } = this.props;
 
-    if (!isDetail) {
-      return null;
-    }
+    const animationStyle = {
+      transform: [{ translateY }],
+    };
 
-    return <View style={styles.toolbarBackground} />;
+    return <Animated.View style={[styles.toolbarBackground, animationStyle]} />;
   }
 }
 
